8f88dbbe105f346503cc7c8b083112951e5bfbc0
Kevin Liu <kevinwliuu@gmail.com>
2024-08-13 10:55:51 -0500

    updating sharing functionality

--- src/components/ScoreComponent.jsx
+++ src/components/ScoreComponent.jsx
@@ -5,19 +5,6 @@ import tecSav from '../pictures/310.Fast-Internet.svg';
 import cozCom from '../pictures/328.Cup-Of-Coffee.svg';
 import effMin from '../pictures/264.Teaming-Up.svg';
 import creCol from '../pictures/33.Details.svg';
-const handleShareFacebook = () => {
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.location.href), '_blank');
-};
-
-const handleShareLinkedIn = () => {
-    window.open('https://www.linkedin.com/sharing/share-offsite/?url=' + encodeURIComponent(document.location.href), '_blank');
-};
-
-const handleCopyLink = () => {
-    navigator.clipboard.writeText(document.location.href)
-        .then(() => alert('Link copied to clipboard!'))
-        .catch(err => alert('Failed to copy link: ', err));
-};
 
 const ScoreComponent = ({ score }) => {
     const getPersonalityType = (score) => {
@@ -32,6 +19,17 @@ const ScoreComponent = ({ score }) => {
                     { type: 'Creative Collectors', description: 'Strategists and Collectors may clash over structure, but finding mutual respect over creativity and innovation can lead to a harmonious home.', imageUrl: creCol }
                 ]
             };
+        } else if (score >= 19 && score <= 24) {
+            return {
+                imageUrl: cozCom,
+                type: 'Cozy Comforter',
+                blurb: 'You’re a master of creating a warm, inviting environment where comfort reigns supreme. Your home feels like a hug, with plush sofas, soft throws, and a steady supply of hot beverages. Your focus is on making sure space is perfect for relaxation, whether it’s a nook for reading or a spot for gathering with friends. Your ability to make people feel at ease and your dedication to comfort make your home a true sanctuary where everyone feels welcome.',
+                compatibilities: [
+                    { type: 'Tech-Savvy Strategists', description: 'If Strategists can learn to appreciate the warmth of Comforters’ coziness, this could be the perfect pairing. Find common ground by incorporating cozy tech solutions that make your home more inviting.', imageUrl: tecSav },
+                    { type: 'Efficient Minimalists', description: 'While Minimalists don’t always focus on comfort, they can appreciate the warmth that Comforters bring. By integrating Comforters\' cozy elements into a minimalist framework, you can create a space that balances simplicity with inviting warmth.', imageUrl: effMin },
+                    { type: 'Creative Collectors', description: 'Collectors and Comforters can create a lively, warm home full of personal touches and creative expressions. This duo likely throws the best parties in town!', imageUrl: creCol }
+                ]
+            };
         } else if (score >= 13 && score <= 18) {
             return {
                 imageUrl: effMin,
@@ -43,17 +41,6 @@ const ScoreComponent = ({ score }) => {
                     { type: 'Creative Collectors', description: "Minimalists might find Collectors' spontaneity challenging, but focusing on each others’ strengths to build a vibrant home filled with both practicality and creativity will bring you happiness.", imageUrl: creCol }
                 ]
             };
-        } else if(score >= 19 && score <= 24) {
-            return {
-                imageUrl: cozCom,
-                type: 'Cozy Comforter',
-                blurb: 'You’re a master of creating a warm, inviting environment where comfort reigns supreme. Your home feels like a hug, with plush sofas, soft throws, and a steady supply of hot beverages. Your focus is on making sure space is perfect for relaxation, whether it’s a nook for reading or a spot for gathering with friends. Your ability to make people feel at ease and your dedication to comfort make your home a true sanctuary where everyone feels welcome.',
-                compatibilities: [
-                    { type: 'Tech-Savvy Strategists', description: 'If Strategists can learn to appreciate the warmth of Comforters\’ coziness, this could be the perfect pairing. Find common ground by incorporating cozy tech solutions that make your home more inviting.', imageUrl: tecSav },
-                    { type: 'Efficient Minimalists', description: 'While Minimalists don’t always focus on comfort, they can appreciate the warmth that Comforters bring. By integrating Comforters\' cozy elements into a minimalist framework, you can create a space that balances simplicity with inviting warmth.', imageUrl: effMin },
-                    { type: 'Creative Collectors', description: 'Collectors and Comforters can create a lively, warm home full of personal touches and creative expressions. This duo likely throws the best parties in town!', imageUrl: creCol }
-                ]
-            };
         } else {
             return {
                 imageUrl: creCol,
@@ -61,40 +48,71 @@ const ScoreComponent = ({ score }) => {
                 blurb: 'Your home is a canvas for your imagination, bursting with vibrant colors, eclectic decor, and one-of-a-kind treasures. You embrace spontaneity and creativity, making your space a unique reflection of your dynamic personality. Each item in your home tells a story, from quirky finds at flea markets to handmade crafts. Your approach to home management is anything but conventional; you thrive on unexpected details and creative touches. While your space might not always adhere to strict organization, its energy and character make it a truly special place, brimming with personality and charm.',
                 compatibilities: [
                     { type: 'Tech-Savvy Strategists', description: 'Strategists and Collectors may clash over structure, but finding mutual respect over creativity and innovation can lead to a harmonious home.', imageUrl: tecSav },
-                    { type: 'Efficient Minimalists', description: 'Minimalists might find Collectors\' spontaneity challenging, but focusing on each others\’ strengths to build a vibrant home filled with both practicality and creativity will bring you happiness.', imageUrl: effMin },
+                    { type: 'Efficient Minimalists', description: 'Minimalists might find Collectors\' spontaneity challenging, but focusing on each others’ strengths to build a vibrant home filled with both practicality and creativity will bring you happiness.', imageUrl: effMin },
                     { type: 'Cozy Comforters', description: 'Collectors and Comforters can create a lively, warm home full of personal touches and creative expressions. This duo likely throws the best parties in town!', imageUrl: cozCom }
                 ]
             };
         }
     };
-    console.log(score)
+
     const personality = getPersonalityType(score);
+
+    const getShareText = () => {
+        const link = "https://liukevinn.github.io/coexistCompatabilityChallenge/";
+        switch (personality.type) {
+            case 'Tech-Savvy Strategist':
+                return `Tech-Savvy Strategist\nI just took a quiz to find out my home management style, and it turns out I’m a Tech-Savvy Strategist! Which means I love using gadgets and planning to keep everything running smoothly at home. 🏡 Think you’re the same or different? Give it a try! ${link}`;
+            case 'Cozy Comforter':
+                return `Cozy Comforter\nI just took a quiz to find out my home management style, and it turns out I’m a Cozy Comforter! Which means my home is all about warmth and inviting vibes. 🛋 Think you’re the same or different? Give it a try! ${link}`;
+            case 'Efficient Minimalist':
+                return `Efficient Minimalist\nI just took a quiz to find out my home management style, and it turns out I’m an Efficient Minimalist! Which means I keep things simple and functional at home. 🧹 Think you’re the same or different? Give it a try! ${link}`;
+            case 'Creative Collector':
+                return `Creative Collector\nI just took a quiz to find out my home management style, and it turns out I’m a Creative Collector! Which means my home is a canvas filled with unique treasures. 🎨 Think you’re the same or different? Give it a try! ${link}`;
+            default:
+                return '';
+        }
+    };
+
+    const handleShareFacebook = () => {
+        const shareText = getShareText();
+        window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(shareText), '_blank');
+    };
+
+    const handleShareLinkedIn = () => {
+        const shareText = getShareText();
+        window.open('https://www.linkedin.com/sharing/share-offsite/?url=' + encodeURIComponent(shareText), '_blank');
+    };
+
+    const handleCopyLink = () => {
+        const currentUrl = document.location.href;
+        navigator.clipboard.writeText(currentUrl)
+            .then(() => alert('Link copied to clipboard!'))
+            .catch(err => alert('Failed to copy link: ', err));
+    };
 
     return (
         <div>
             <div className={styles.container}>
-    <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)', textAlign: 'center', maxWidth: '500px' }}>
-        <h2 style={{ textTransform: 'lowercase' }}>
-            your result:<br />
-            {personality.type}
-        </h2>
-    
+                <div style={{ backgroundColor: 'white', padding: '20px', borderRadius: '8px', boxShadow: '0 2px 10px rgba(0, 0, 0, 0.1)', textAlign: 'center', maxWidth: '500px' }}>
+                    <h2 style={{ textTransform: 'lowercase' }}>
+                        your result:<br />
+                        {personality.type}
+                    </h2>
 
-    <img src={personality.imageUrl} className={styles.compatibilityImage} />
-    
-    
-    <p className={styles.description}>{personality.blurb}</p>
-    </div>
+                    <img src={personality.imageUrl} className={styles.compatibilityImage} alt={personality.type} />
 
-    <div className={styles.shareContainer}>
-        <h2>share your results:</h2>
-        <div className={styles.icons}>
-            <button onClick={handleShareFacebook}><Facebook /></button>
-            <button onClick={handleShareLinkedIn}><LinkedIn /></button>
-            <button onClick={handleCopyLink}><Link /></button>
-        </div>
-    </div>
-</div>
+                    <p className={styles.description}>{personality.blurb}</p>
+                </div>
+
+                <div className={styles.shareContainer}>
+                    <h2>share your results:</h2>
+                    <div className={styles.icons}>
+                        <button onClick={handleShareFacebook}><Facebook /></button>
+                        <button onClick={handleShareLinkedIn}><LinkedIn /></button>
+                        <button onClick={handleCopyLink}><Link /></button>
+                    </div>
+                </div>
+            </div>
 
             <div className={styles.compatibility}>
                 <h2>your compatibility with...</h2>
